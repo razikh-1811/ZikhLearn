7c9870cd341a7a116e79523e1968f2cbdb193621
Shaik Mahaboob Razikh <skrazikh1811@gmail.com>
2026-01-28 09:52:34 +0530

    Pass template option to getToken function

--- elearning/src/Pages/Analytics.jsx
+++ elearning/src/Pages/Analytics.jsx
@@ -28,7 +28,7 @@ const Analytics = () => {
           return;
         }
 
-        const token = await getToken();
+        const token = await getToken({template:"backend"});
 
         const res = await axios.get(
           `${import.meta.env.VITE_API_URL}/api/courses/instructor/stats`,
